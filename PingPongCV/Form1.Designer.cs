3495d5b9764fe504804a0b07d0fa8648698f9552
saige kumar <saigekumar2009@gmail.com>
2023-12-29 16:51:33 -0800

    ping pong base game

---
+++ PingPongCV/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace PingPongCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.playerScore = new System.Windows.Forms.Label();
+            this.computerScore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(51, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(802, 395);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // playerScore
+            // 
+            this.playerScore.AutoSize = true;
+            this.playerScore.Location = new System.Drawing.Point(676, 33);
+            this.playerScore.Name = "playerScore";
+            this.playerScore.Size = new System.Drawing.Size(32, 25);
+            this.playerScore.TabIndex = 1;
+            this.playerScore.Text = "00";
+            // 
+            // computerScore
+            // 
+            this.computerScore.AutoSize = true;
+            this.computerScore.Location = new System.Drawing.Point(139, 33);
+            this.computerScore.Name = "computerScore";
+            this.computerScore.Size = new System.Drawing.Size(32, 25);
+            this.computerScore.TabIndex = 1;
+            this.computerScore.Text = "00";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(894, 479);
+            this.Controls.Add(this.computerScore);
+            this.Controls.Add(this.playerScore);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label playerScore;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label computerScore;
+    }
+}
+
---
+++ PingPongCV/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingPongCV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Rectangle player;
+        private Rectangle computer;
+        private Rectangle ball;
+
+        private int paddleSpeed;
+        private int computerSpeed;
+        private int xSpeed;
+        private int ySpeed;
+        private int computerPoints;
+        private int playerPoints;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gameTimer.Enabled = true;
+
+            computer = new Rectangle(5, pictureBox1.Height / 2 - 40, 20, 80);
+            player = new Rectangle(pictureBox1.Width - 25, pictureBox1.Height / 2 - 40, 20, 80);
+            ball = new Rectangle(pictureBox1.Width / 2, pictureBox1.Height / 2, 20, 20);
+
+            xSpeed = 5;
+            ySpeed = 5;
+            computerSpeed = 10;
+            paddleSpeed = 10;
+            playerPoints = 0;
+            computerPoints = 0;
+        }
+
+        private void gameTimer_Tick_1(object sender, EventArgs e)
+        {
+            if (computerPoints == 10 || playerPoints == 10)
+            {
+                playerPoints = 0;
+                computerPoints = 0;
+
+                ball.X = pictureBox1.Width / 2;
+                ball.Y = pictureBox1.Height / 2;
+
+                computer.X = 5;
+                computer.Y = pictureBox1.Height / 2 - 40;
+
+                player.X = pictureBox1.Width - 25;
+                player.Y = pictureBox1.Height / 2 - 40;
+            }
+
+
+            if (ball.X <= 0)
+            {
+                playerPoints++;
+                ball.X = pictureBox1.Width / 2;
+                ball.Y = pictureBox1.Height / 2;
+            }
+            if (ball.X >= pictureBox1.Width - 25)
+            {
+                computerPoints++;
+                ball.X = pictureBox1.Width / 2;
+                ball.Y = pictureBox1.Height / 2;
+            }
+
+            computerScore.Text = $"{computerPoints}";
+            playerScore.Text = $"{playerPoints}";
+
+            if (ball.Y <= 0 || ball.Y >= pictureBox1.Height)
+            {
+                ySpeed *= -1;
+            }
+
+            ball.X += xSpeed;
+            ball.Y += ySpeed;
+
+            if (computer.Bottom >= pictureBox1.Height || computer.Top <= 0)
+            {
+                computerSpeed *= -1;
+            }
+
+            computer.Y += computerSpeed;
+
+            if (ball.IntersectsWith(player))
+            {
+                ball.X -= 5;
+                xSpeed *= -1;
+            }
+            if (ball.IntersectsWith(computer))
+            {
+                ball.X += 5;
+                xSpeed *= -1;
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.S || e.KeyCode == Keys.Down)
+            {
+                if (player.Bottom <= pictureBox1.Height)
+                {
+                    player.Y += paddleSpeed;
+                }
+            }
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.Up)
+            {
+                if (player.Top >= 0)
+                {
+                    player.Y -= paddleSpeed;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.RoyalBlue, computer);
+            e.Graphics.FillRectangle(Brushes.Red, player);
+            e.Graphics.FillEllipse(Brushes.Black, ball);
+        }
+
+
+    }
+}
